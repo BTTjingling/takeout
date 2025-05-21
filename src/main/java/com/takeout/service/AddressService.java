e129d1e87a3ea6b08420e4f8320ae01a7fc016e7
Meci-yu <342948980@qq.com>
2025-05-21 16:30:25 +0800

    完善收货地址

--- src/main/java/com/takeout/service/AddressService.java
+++ src/main/java/com/takeout/service/AddressService.java
@@ -9,4 +9,13 @@ public interface AddressService extends IService<Address> {
     List<Address> getUserAddresses(Long userId);
 
     void setDefaultAddress(Long userId, Long addressId);
+
+    // 新增用户地址
+    void addUserAddress(Long userId, Address address);
+
+    // 修改用户地址
+    void updateUserAddress(Long addressId, Address address);
+
+    // 删除用户地址
+    void deleteUserAddress(Long addressId);
 }
